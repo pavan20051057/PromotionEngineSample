c9da10f94933f0187efa2a1c8d945f66c670902f
pavan20051057 <92031756+pavan20051057@users.noreply.github.com>
2021-10-06 19:15:32 +0530

    Added files for the project

---
+++ ClassLibraryPromotionEngine/Controlls/Engine.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryPromotionEngine
+{
+  public class Engine
+  {
+    private IEnumerable<SKU_Price> PriceList;
+    private IEnumerable<Promotion> Promotions;
+
+    public Engine(IEnumerable<SKU_Price> priceList, IEnumerable<Promotion> promotions)
+    {
+      this.PriceList = priceList;
+      this.Promotions = promotions;
+    }
+
+    public void CheckOut(Order order)
+    {
+      var foundItems = new List<Item>();
+      if (Promotions != null && Promotions.Count() > 0)
+        foreach (var promotion in Promotions)
+        {
+          var validatedItems = promotion.Validate(order, foundItems);
+          UpdateValidatedItems(foundItems, validatedItems);
+        }
+
+      ApplyRegularPrice(order, foundItems);
+    }
+
+    private void ApplyRegularPrice(Order order, List<Item> foundItems)
+    {
+      foreach (var item in order.Items)
+      {
+        var validateItem = foundItems.FirstOrDefault(x => x.SKU_Id == item.SKU_Id) ?? item;
+        var quantity = validateItem.Quantity;
+        if (quantity > 0)
+          order.TotalAmount += quantity * PriceList.First(x => x.SKU_Id == item.SKU_Id).UnitPrice;
+      }
+    }
+
+    private static void UpdateValidatedItems(List<Item> foundItems, IEnumerable<Item> validatedItems)
+    {
+      if (validatedItems == null || validatedItems.Count() < 1)
+        return;
+
+      foreach (var item in validatedItems)
+        if (!foundItems.Any(x => x.SKU_Id == item.SKU_Id))
+          foundItems.Add(item);
+    }
+  }
+}
---
+++ ClassLibraryPromotionEngine/Models/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryPromotionEngine
+{
+  public class Item
+  {
+    public Item() { }
+    public Item(Item item)
+    {
+      SKU_Id = item.SKU_Id;
+      Quantity = item.Quantity;
+    }
+
+    public char SKU_Id { get; set; }
+    public int Quantity { get; set; }
+
+    public override string ToString()
+    {
+      return $"{SKU_Id} {Quantity}";
+    }
+  }
+}
